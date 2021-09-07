4de5372c5e54ed217bfc36285693a99f4c394c37
Teresa Rosinski <TeresaRosinski@users.noreply.github.com>
2021-09-07 13:17:34 -0500

    functionality to filter arts section based on use rinput" "

--- scripts.js
+++ scripts.js
@@ -1,59 +1,64 @@
 const key = "L6hLw10R0bLGztF9APZn68K21ZWbFlAC";
-const topic = "arts";
 
+//Arts Search Form Code
+const artsSearchForm = document.querySelector("#artsSearchForm");
+
+artsSearchForm.addEventListener("submit", async function(e){
+  e.preventDefault();
+
+  const searchTerm = artsSearchForm.elements.query.value;
+  
+  const res = await axios.get(
+		`https://api.nytimes.com/svc/topstories/v2/arts.json?api-key=${key}`
+	);
+ 
+  const arrayResults = res.data.results;
+  const filteredRes = arrayResults.filter(result => result.title.includes(searchTerm))
+  console.log('filtered ', filteredRes);
+  makeCards(filteredRes, 'arts');
+  })
+  
+
+//Function to dynamically create event listeners for specific search forms base don section name
 //API CALL -- > Retrieves articles according to specific topic
 async function grabArticles(key, topic) {
-  const res = await axios.get(
-    `https://api.nytimes.com/svc/topstories/v2/${topic}.json?api-key=${key}`
-  );
-  let arrayResults = res.data.results;
-  //makes cards according to specific topic
-  makeCards(arrayResults, `${topic}`);
-}
+	const res = await axios.get(
+		`https://api.nytimes.com/svc/topstories/v2/${topic}.json?api-key=${key}`
+	);
+	let arrayResults = res.data.results;
 
-//Makes title for each topic -- successfully creates list of all topics
-function makeSections(topicList) {
-  allStoriesContainer = document.body.querySelector("#newsStories");
-  for (let topic of topicList) {
-    let topicContainer = document.createElement("DIV");
-    topicContainer.id = `${topic}`;
-    const topicTitle = document.createElement("P");
-    topicTitle.className = `${topic}Title`;
-    topicTitle.innerHTML = `${topic}`;
-    topicContainer.appendChild(topicTitle);
-    allStoriesContainer.appendChild(topicContainer);
-  }
+	//makes cards according to specific topic
+	makeCards(arrayResults, `${topic}`);
 }
 
 function makeCards(results, topic) {
-  console.log("topic:", topic);
-  console.log("results:", results);
-  let section = document.body.querySelector(`#${topic}`);
-  for (let article of results) {
-    const card = document.createElement("A");
-    const textBox = document.createElement("DIV");
-    const img = document.createElement("IMG");
-    const title = document.createElement("P");
-    const authors = document.createElement("P");
-    title.innerHTML = article.title;
-    authors.innerHTML = article.byline;
-    img.src = article.multimedia[4].url;
-    card.href = article.short_url;
-    card.className = "card";
-    textBox.className = "textBox";
-    title.className = "title";
-    textBox.appendChild(title);
-    textBox.appendChild(authors);
-    card.appendChild(img);
-    card.appendChild(textBox);
-    section.appendChild(card);
-  }
+	let section = document.body.querySelector(`#${topic}`);
+	for (let article of results) {
+		const card = document.createElement("A");
+		const textBox = document.createElement("DIV");
+		const img = document.createElement("IMG");
+		const title = document.createElement("P");
+		const authors = document.createElement("P");
+		title.innerHTML = article.title;
+		authors.innerHTML = article.byline;
+		img.src = article.multimedia[4].url;
+		card.href = article.short_url;
+		card.className = "card";
+		textBox.className = "textBox";
+		title.className = "title";
+		textBox.appendChild(title);
+		textBox.appendChild(authors);
+		card.appendChild(img);
+		card.appendChild(textBox);
+		section.appendChild(card);
+	}
 }
 
 const topicsArray = [
-  "arts",
-  "automobiles",
-  "books",
+	"arts",
+	"automobiles",
+	"books",
+	/*
   "business",
   "fashion",
   "food",
@@ -76,10 +81,13 @@ const topicsArray = [
   "travel",
   "upshot",
   "us",
-  "world",
+  "world", */
 ];
 
-makeSections(topicsArray);
+/*
 grabArticles(key, topicsArray[0]);
+
+
 grabArticles(key, topicsArray[1]);
 grabArticles(key, topicsArray[2]);
+*/
