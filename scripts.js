a9433085928500c39949c013998d8a908a25f4d8
Teresa Rosinski <TeresaRosinski@users.noreply.github.com>
2021-09-09 11:30:30 -0500

    attempt to append no return values'

--- scripts.js
+++ scripts.js
@@ -5,20 +5,25 @@ const artsSearchForm = document.querySelector("#artsSearchForm");
 
 artsSearchForm.addEventListener("submit", async function (e) {
 	e.preventDefault();
-
 	const searchTerm = artsSearchForm.elements.query.value.toUpperCase();
 	const searchInput = document.querySelector('#searchArts');
-	console.log(searchInput);
-
 	const res = await axios.get(
 		`https://api.nytimes.com/svc/topstories/v2/arts.json?api-key=${key}`
 	);
-
 	const arrayResults = res.data.results;
 	const filteredRes = arrayResults.filter((result) =>
 		result.title.toUpperCase().includes(searchTerm)
 	);
-	console.log("filtered ", filteredRes);
+
+	
+		//if the searched results returned empty - return a card that says, search results for 'searchTerm' returned no resutls - specific to arts section and arts design
+	console.log('filt length', filteredRes.length);	
+	if(filteredRes.length === 0) 
+		{
+			makeNoResCard(searchTerm);
+			console.log("ran");
+		} 
+
 	makeCards(filteredRes, "arts");
 	searchInput.value = " ";
 });
@@ -26,17 +31,24 @@ artsSearchForm.addEventListener("submit", async function (e) {
 //Function to dynamically create event listeners for specific search forms base don section name
 //API CALL -- > Retrieves articles according to specific topic
 async function grabArticles(key, topic) {
+
+	//async api call
 	const res = await axios.get(
 		`https://api.nytimes.com/svc/topstories/v2/${topic}.json?api-key=${key}`
 	);
+
+	// creating an array from returned API call
 	let arrayResults = res.data.results;
 	
 	//makes cards according to specific topic
 	makeCards(arrayResults, `${topic}`);
 }
 
+
+//Creates success cards
 function makeCards(results, topic) {
 	let section = document.body.querySelector(`#${topic}ResultsAll`);
+	//clears results that are already populating the section - important for search feature or card are added onto the previous results
 	section.innerHTML = "";
 	for (let article of results) {
 		const card = document.createElement("A");
@@ -57,6 +69,18 @@ function makeCards(results, topic) {
 		card.appendChild(textBox);
 		section.appendChild(card);
 	}
+}
+function makeNoResCard (searchTerm) {
+	const section = document.body.querySelector('#artsResultsAll');
+	const card = document.createElement("DIV");
+	const text = document.createElement("P");
+	text.innerHTML = `There are no current results for ${searchTerm}`;
+	text.className = "title";
+	card.className = "card";
+	card.appendChild(text);
+	section.appendChild(card);
+	console.log(card);
+	console.log(section);
 }
 
 const topicsArray = [
